0308d2da4520a0a1b258883d33a31f389ec32335
ganomin <szymon.wirkus12@gmail.com>
2025-01-02 15:43:09 +0000

    sm6115-common: Nuke UDFPS
    * None of the sm6115 targets uses udfps
    
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,17 +50,9 @@ TARGET_USES_GRALLOC1 := true
 TARGET_USES_GRALLOC4 := true
 TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
-ifeq ($(TARGET_HAS_UDFPS),true)
-TARGET_USES_FOD_ZPOS := true
-endif
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
-
-# Fingerprint
-ifeq ($(TARGET_HAS_UDFPS),true)
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
-endif
 
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_bengal
--- bengal.mk
+++ bengal.mk
@@ -150,11 +150,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-ifeq ($(TARGET_HAS_UDFPS),true)
-PRODUCT_PACKAGES += \
-    libudfpshandler
-endif
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti

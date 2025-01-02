c669210b224a203f45185c09e74ab12db32f2c8a
Kelvin Zhang <zhangkelvin@google.com>
2025-01-02 15:41:32 +0000

    sm6115-common: Explicitly set as an non-AB target
    * We will be changing AB to be the default, existing non-AB targets
      need to explicitly set AB_OTA_UPDATER to false
    
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- bengal.mk
+++ bengal.mk
@@ -10,6 +10,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# A/B
+AB_OTA_UPDATER := false
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
